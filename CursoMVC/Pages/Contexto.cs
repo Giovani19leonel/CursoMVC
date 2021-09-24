65c635f6d7b1efcffe69e76903873547bad9fb71
Giovani Henrique <giovani142002@hotmail.com>
2021-09-24 01:47:52 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Pages/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Pages
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<CursoMVC.Pages.Shared.Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost\SQLEXPRESS;Database=CursoMVC;Trusted_Connection=True;");
+        }
+    }
+}
